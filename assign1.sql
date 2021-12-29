6e8adba2f4afca10fd1d77b5e8ff07c09455e2bb
Rahul Awari <47710382+awarirahul365@users.noreply.github.com>
2021-12-29 14:47:28 +0530

    Create assign1.sql

---
+++ assign1.sql
@@ -0,0 +1,20 @@
+create database if not exists training;
+use training;
+DROP table if exists demography;
+create table demography(
+	CustId int auto_increment primary key,
+    Full_Name varchar(255),
+    Age int,
+    Gender varchar(1)
+);
+insert into demography(Full_Name,Age,Gender) Values ('john',25,'M');
+insert into demography(Full_Name,Age,Gender) Values ('Pawan',26,'M');
+insert into demography(Full_Name,Age,Gender) Values ('Hema',31,'F');
+insert into demography(Full_name,Gender) values ('Rekha','F');
+SET SQL_SAFE_UPDATES = 0;
+UPDATE demography
+SET Age=null
+WHERE Full_name='john';
+SET SQL_SAFE_UPDATES = 1;
+select * from demography
+WHERE Age is null;
